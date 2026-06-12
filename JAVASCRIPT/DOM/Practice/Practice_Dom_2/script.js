3344f37538d00ef8997d1323dcadcfce871cfee2
Sakshee Sachan <sakshee.sachan003@gmail.com>
2026-06-12 11:43:19 +0530

    added structure of practice question

---
+++ JAVASCRIPT/DOM/Practice/Practice_Dom_2/script.js
@@ -0,0 +1,11 @@
+const main = document.querySelector("main");
+
+const box1 = document.querySelector(".box1");
+const box2 = document.querySelector(".box2");
+const box3 = document.querySelector(".box3");
+
+box2.style.backgroundColor = "blue";
+
+box3.style.backgroundColor = "yellow";
+
+// inserBefore() ->
