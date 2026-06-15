2d108d255526d38d6708bae729fa794edeb7bdf5
Sakshee Sachan <sakshee.sachan003@gmail.com>
2026-06-15 21:26:57 +0530

    dom practice 2

--- JAVASCRIPT/DOM/Practice/Practice_Dom_2/script.js
+++ JAVASCRIPT/DOM/Practice/Practice_Dom_2/script.js
@@ -1,11 +1,47 @@
 const main = document.querySelector("main");
 
-const box1 = document.querySelector(".box1");
-const box2 = document.querySelector(".box2");
-const box3 = document.querySelector(".box3");
+/////// Option 1 :) selecting element and then
 
-box2.style.backgroundColor = "blue";
+// const box1 = document.querySelector(".box1");
+// const box2 = document.querySelector(".box2");
+// const box3 = document.querySelector(".box3");
 
-box3.style.backgroundColor = "yellow";
+// box2.style.backgroundColor = "blue";
 
-// inserBefore() ->
+// box3.style.backgroundColor = "yellow";
+
+///// Option - 2 :)  Create box and then in in main
+
+const box1 = document.createElement("div");
+const box2 = document.createElement("div");
+const box3 = document.createElement("div");
+
+box1.classList.add("box");
+box2.classList.add("box");
+box3.classList.add("box");
+
+box1.style.backgroundColor = "red";
+box2.style.backgroundColor = "green";
+box3.style.backgroundColor = "blue";
+
+main.append(box1);
+main.append(box2);
+main.append(box3);
+
+//// Prepend () -> insert always before(sabse pahle)
+// main.prepend(box3);
+
+//// insertBefore() -> insert box2 from before box1
+// main.insertBefore(box2, box1);
+
+//// before() -> always before
+// main.before(box2);
+
+//// after() -> always last
+// box2.after(box1);
+
+//// replaceChild() -> replace child
+// main.replaceChild(box3, box1);
+
+//// replaceWith() -> replace content
+box3.replaceWith(box1);
