3292c414d5e9f03e3c23ab5db856e50dabc79496
Sakshee Sachan <sakshee.sachan003@gmail.com>
2026-06-27 20:50:55 +0530

    Added react day 1 concepts

--- React_Everything/React_Day_1/script.js
+++ React_Everything/React_Day_1/script.js
@@ -7,11 +7,46 @@ document.body.append(h1);
 
 console.log("Real DOM :) ", h1);
 
-//// Creates Virtual DOM
-let h2 = React.createElement(
+//// Creates Virtual DOM Using React
+let rh1 = React.createElement(
   "h1",
   { class: "heading" },
   "Hello I'm Virtual DOM",
 );
 
-console.log("Virtual DOM :) ", h2);
+// if don't want to give attribute then pass null
+let rh2 = React.createElement("h2", null, "Hello I'm Virtual DOM");
+
+// if give span, then use again createElement
+let rh3 = React.createElement(
+  "h3",
+  null,
+  React.createElement("span", {}, "Hello, I'm span"),
+);
+
+console.log("Virtual DOM :) ", rh1);
+console.log("Virtual DOM :) ", rh2);
+console.log("Virtual DOM :) ", rh3);
+
+/////// Append Vrirtual DOM into Real DOM ( using ReactDOM)
+
+console.log(ReactDOM);
+
+let realDomElem = document.querySelector("#root");
+
+let rootOfReact = ReactDOM.createRoot(realDomElem);
+
+// rootOfReact.render(rh3);
+
+////// Div -> h1 -> span
+
+let div = React.createElement("div", {}, [
+  React.createElement("h2", {}, React.createElement("span", {}, "I'm Span ")),
+  React.createElement(
+    "h3",
+    {},
+    React.createElement("span", {}, "I'm Span in h2"),
+  ),
+]);
+
+ReactDOM.createRoot(realDomElem).render(div);
